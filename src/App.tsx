a7ae4e03493afd264939eb645367b01798bb9125
ShouvikMisra <shouvik.misra@keross.com>
2026-02-23 12:32:13 +0530

    action-menu component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { ProviderWrapper } from "./ikoncomponents/provider-wrapper";
 import { setIkonConfig } from "./utils/config";
+import { ActionMenu } from "./ikoncomponents/action-menu";
 setIkonConfig({
   IKON_BASE_API_URL: "https://ikoncloud-dev.keross.com/ikon-api",
   IKON_PLATFORM_UI_URL: "/",
@@ -9,7 +10,7 @@ setIkonConfig({
 function App() {
   return (
     <>
-      <ProviderWrapper>Hello World</ProviderWrapper>
+      <ProviderWrapper><ActionMenu actionMenus={[{label: "Lead"}, {label: "Deal"}]}></ActionMenu></ProviderWrapper>
     </>
   );
 }
