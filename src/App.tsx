e7910ee806b0309fea3b5cc667b7df74556a4482
ShouvikMisra <shouvik.misra@keross.com>
2026-02-25 11:58:49 +0530

    phone input implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { ProviderWrapper } from "./ikoncomponents/provider-wrapper";
 import { setIkonConfig } from "./utils/config";
+import { PhoneInput } from "./ikoncomponents/phone-input";
 setIkonConfig({
   IKON_BASE_API_URL: "https://ikoncloud-dev.keross.com/ikon-api",
   IKON_PLATFORM_UI_URL: "/",
