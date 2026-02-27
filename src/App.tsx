cc91ab4a7617abcfb3c97dc0b206f613abbf5387
MadhuDey2942 <madhurima.dey@keross.com>
2026-02-27 12:21:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import { useState } from "react";
 import { ProviderWrapper } from "./ikoncomponents/provider-wrapper";
 import { setIkonConfig } from "./utils/config";
+import { UploadTab } from "./ikoncomponents/upload-tab";
 setIkonConfig({
   IKON_BASE_API_URL: "https://ikoncloud-dev.keross.com/ikon-api",
   IKON_PLATFORM_UI_URL: "/",
