1b88d550c0f8b708e1e80a2ff198206935909671
MadhuDey2942 <madhurima.dey@keross.com>
2026-03-04 12:21:53 +0530

    removed theme-switcher, added overflow-hidden to root, and wrote index.ts and also updated others

---
+++ src/index.ts
@@ -0,0 +1,232 @@
+import "./styles.css"; // just imports, no processing
+export * from "./shadcn/ui/accordion";
+export * from "./shadcn/ui/alert-dialog";
+export * from "./shadcn/ui/alert";
+export * from "./shadcn/ui/avatar";
+export * from "./shadcn/ui/badge";
+export * from "./shadcn/ui/button";
+export * from "./shadcn/ui/calendar";
+export * from "./shadcn/ui/card";
+export * from "./shadcn/ui/checkbox";
+export * from "./shadcn/ui/command";
+export * from "./shadcn/ui/dialog";
+export * from "./shadcn/ui/dropdown-menu";
+export * from "./shadcn/ui/hover-card";
+export * from "./shadcn/ui/input";
+export * from "./shadcn/ui/label";
+export * from "./shadcn/ui/navigation-menu";
+export * from "./shadcn/ui/popover";
+export * from "./shadcn/ui/progress";
+export * from "./shadcn/ui/radio-group";
+export * from "./shadcn/ui/scroll-area";
+export * from "./shadcn/ui/select";
+export * from "./shadcn/ui/separator";
+export * from "./shadcn/ui/sheet";
+export * from "./shadcn/ui/sidebar";
+export * from "./shadcn/ui/skeleton";
+export * from "./shadcn/ui/slider";
+export * from "./shadcn/ui/sonner";
+export * from "./shadcn/ui/switch";
+export * from "./shadcn/ui/table";
+export * from "./shadcn/ui/tabs";
+export * from "./shadcn/ui/textarea";
+export * from "./shadcn/ui/tooltip";
+
+export * from "./shadcn/ui/aspect-ratio";
+export * from "./shadcn/ui/breadcrumb";
+export * from "./shadcn/ui/collapsible";
+export * from "./shadcn/ui/drawer";
+export * from "./shadcn/ui/form";
+export * from "./shadcn/ui/input-otp";
+export * from "./shadcn/ui/toggle-group";
+export * from "./shadcn/ui/toggle";
+
+export { ActionMenu } from "./ikoncomponents/action-menu";
+export type {
+  ActionMenuProps,
+  ExtraActionParams,
+} from "./ikoncomponents/action-menu/type";
+export { CustomAlertDialog } from "./ikoncomponents/alert-dialog";
+export {
+  useDialog,
+  DialogProvider,
+} from "./ikoncomponents/alert-dialog/dialog-context";
+export {
+  TextButton,
+  TextButtonWithTooltip,
+  IconTextButton,
+  IconTextButtonWithTooltip,
+  IconButton,
+  IconButtonWithTooltip,
+} from "./ikoncomponents/buttons";
+export type {
+  ButtonProps,
+  ButtonWithTooltipProps,
+} from "./ikoncomponents/buttons";
+export { ComboboxInput } from "./ikoncomponents/combobox-input";
+export type {
+  ComboBoxInputProps,
+  ComboboxItemProps,
+} from "./ikoncomponents/combobox-input/type";
+export { DataTableColumnFilter } from "./ikoncomponents/data-table/datatable-column-filter";
+export { DataTableFacetedFilter } from "./ikoncomponents/data-table/datatable-faceted-filter";
+export { DataTableFilterMenu } from "./ikoncomponents/data-table/datatable-filter-menu";
+export {
+  convertFileToObject,
+  FileUploader,
+  getImageFromObject,
+} from "./ikoncomponents/fileUpload";
+export type { FileUploaderProps } from "./ikoncomponents/fileUpload";
+export { DataTablePagination } from "./ikoncomponents/data-table/datatable-pagination";
+export { DataTableToolbar } from "./ikoncomponents/data-table/datatable-toolbar";
+export { getDataTableColumnTitle } from "./ikoncomponents/data-table/function";
+export { DataTable } from "./ikoncomponents/data-table";
+export type {
+  DataTableProps,
+  DTColumnsProps,
+  DTExtraParamsProps,
+  DTActionMenuProps,
+  DataTableViewOptionsProps,
+  DTToolBarProps,
+  DataTableFilterProps,
+  DataTableFacetedFilterProps,
+  DataTablePaginationProps,
+  DragDropHeaderProp,
+} from "./ikoncomponents/data-table/type";
+export { EChart } from "./ikoncomponents/e-chart";
+export { FileInput } from "./ikoncomponents/file-input";
+export { GlowingEffect } from "./ikoncomponents/glowing-effect";
+export { Icon } from "./ikoncomponents/icon";
+export { LoadingSpinner } from "./ikoncomponents/loading-spinner";
+export type {
+  ISVGProps,
+  LoadingSpinnerProps,
+} from "./ikoncomponents/loading-spinner";
+export { MultiCombobox } from "./ikoncomponents/multi-combobox";
+export { NoDataComponent } from "./ikoncomponents/no-data";
+export { PageWrapper } from "./ikoncomponents/page-wrapper";
+export { PasswordStrengthMeter } from "./ikoncomponents/password-strength-meter";
+export { PhoneInput } from "./ikoncomponents/phone-input";
+export { SearchInput } from "./ikoncomponents/search-input";
+export { SheetComponent } from "./ikoncomponents/sheet";
+export { SimpleWidget } from "./ikoncomponents/simple-widget";
+export { CustomTabs } from "./ikoncomponents/tabs";
+export type { TabArray, TabProps } from "./ikoncomponents/tabs/type";
+export { ThemeToggleBtn } from "./ikoncomponents/theme-toggle-btn";
+export { TitleProgress } from "./ikoncomponents/title-progress";
+export { TooltipComponent } from "./ikoncomponents/tooltip";
+export { FrameworkItemDropdown } from "./ikoncomponents/twolevel-dropdown";
+export type {
+  FrameworkEntry,
+  TreeNode,
+  ParentEntry,
+  ProcessedFrameworkData,
+} from "./ikoncomponents/twolevel-dropdown";
+export { Widgets } from "./ikoncomponents/widgets";
+export type {
+  WidgetProps,
+  WidgetsFunctionProps,
+} from "./ikoncomponents/widgets/type";
+export { BigCalendar } from "./ikoncomponents/big-calendar";
+export type {
+  BigCalendarProps,
+  ExtraParamsEvent,
+  BigCalendarEventProps,
+  BigCalenderToolbarProps,
+} from "./ikoncomponents/big-calendar/type";
+
+export {
+  BreadcrumbProvider,
+  useBreadcrumb,
+} from "./ikoncomponents/app-breadcrumb/BreadcrumbProvider";
+export type { BreadcrumbItemProps } from "./ikoncomponents/app-breadcrumb/BreadcrumbProvider";
+export {
+  AppBreadcrumb,
+  RenderAppBreadcrumb,
+} from "./ikoncomponents/app-breadcrumb";
+export { FormComboboxInput } from "./ikoncomponents/form-fields/combobox-input";
+export { FormComboboxInputWithValue } from "./ikoncomponents/form-fields/combobox-input-value";
+export { FormDateInput } from "./ikoncomponents/form-fields/date-input";
+export { FormFileInput } from "./ikoncomponents/form-fields/file-input";
+export { FormInput } from "./ikoncomponents/form-fields/input";
+export { FormMultiComboboxInput } from "./ikoncomponents/form-fields/multi-combobox-input";
+export { FormOtpInput } from "./ikoncomponents/form-fields/otp-input";
+export { FormPhoneInput } from "./ikoncomponents/form-fields/phone-input";
+export { FormTextarea } from "./ikoncomponents/form-fields/textarea";
+export type {
+  FormFieldProps,
+  FormInputProps,
+  FormTextareaProps,
+  FormDateInputProps,
+  FormComboboxInputProps,
+  FormComboboxItemProps,
+} from "./ikoncomponents/form-fields/types";
+export { GradeTableLoader } from "./ikoncomponents/skeleton-loader/skeleton-table";
+export type { GradeTableLoaderProps } from "./ikoncomponents/skeleton-loader/skeleton-table";
+export { SkeletonWidget } from "./ikoncomponents/skeleton-loader/skeleton-widget";
+export { UploadTab } from "./ikoncomponents/upload-tab";
+export { AppSidebar } from "./ikoncomponents/main-layout/app-sidebar";
+export { Footer } from "./ikoncomponents/main-layout/footer";
+export { Header } from "./ikoncomponents/main-layout/header";
+export type {
+  Account,
+  Software,
+} from "./ikoncomponents/main-layout/main-sidebar";
+export { MainSidebar } from "./ikoncomponents/main-layout/main-sidebar";
+export { NavMain } from "./ikoncomponents/main-layout/nav-main";
+export type {
+  SidebarNavSubItem,
+  SidebarNavItem,
+  SidebarNavContextType,
+} from "./ikoncomponents/main-layout/SidebarNavContext";
+export {
+  SidebarNavProvider,
+  useSidebarNav,
+} from "./ikoncomponents/main-layout/SidebarNavContext";
+export { MainLayout } from "./ikoncomponents/main-layout";
+export { ProviderWrapper } from "./ikoncomponents/provider-wrapper";
+export { ActivitySheet } from "./ikoncomponents/activity-sheet";
+export type { ActivityLogProps } from "./ikoncomponents/activity-sheet";
+export {
+  ImageCropperProvider,
+  useImageCropper,
+} from "./ikoncomponents/image-cropper-upload";
+export type {
+  ImageCropperProps,
+  OriginalImageProps,
+  AspectRatioWiseImagesProps,
+  ImageCropperContextProps,
+} from "./ikoncomponents/image-cropper-upload";
+export { ImageCropper } from "./ikoncomponents/image-cropper-upload/image-cropper";
+export type { CropperImgProps } from "./ikoncomponents/image-cropper-upload/image-cropper";
+export { CropperFormWithModal } from "./ikoncomponents/image-cropper-upload/cropper-form-with-modal";
+export { CropperForm } from "./ikoncomponents/image-cropper-upload/cropper-form";
+export { NewCropperImg } from "./ikoncomponents/image-cropper-upload/components/newCropper";
+export type { CropperImgProps as NewCropperImgProps } from "./ikoncomponents/image-cropper-upload/components/newCropper";
+export { NewImageForm } from "./ikoncomponents/image-cropper-upload/components/newImageUploadForm";
+export type { ImageFormProps } from "./ikoncomponents/image-cropper-upload/components/newImageUploadForm";
+export { WorkInProgress } from "./ikoncomponents/work-in-progress";
+export { CustomComboboxInput } from "./ikoncomponents/custom-combo-dropdown"
+export { AssistantComponent } from "./ikoncomponents/assistant-ui/Assistant";
+
+export { ThemeProvider } from "./utils/theme-provider";
+export { RadiusProvider, useRadius } from "./utils/border-radius-provider";
+export { FontProvider, useFont } from "./utils/font-provider";
+export { cn } from "./shadcn/lib/utils";
+export type { CookieSessionOptionsProps } from "./utils/session/cookieSession";
+export {
+  setCookieSession,
+  getCookieSession,
+  clearCookieSession,
+  clearAllCookieSession,
+} from "./utils/session/cookieSession";
+export {
+  getValidAccessToken,
+  refreshAccessToken,
+//   decodeAccessToken,
+//   logOut
+} from "./utils/token-management";
+export type { TokenResponse } from "./utils/token-management/types";
+
+export { useIsMobile } from "./shadcn/hooks/use-mobile";
+export {useRefresh} from "./ikoncomponents/main-layout/RefreshContext";
