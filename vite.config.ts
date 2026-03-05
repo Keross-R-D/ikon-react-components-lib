bdb6d60692b5432be24c689af41dcc69465690a9
MadhuDey2942 <madhurima.dey@keross.com>
2026-03-05 12:19:17 +0530

    tried to export the styles.css as styles.css

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,8 @@ export default defineConfig({
     rollupOptions: {
       external: ["react", "react-dom"],
     },
+    cssCodeSplit: false,
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
