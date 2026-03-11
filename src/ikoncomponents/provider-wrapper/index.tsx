d12997351f6f91be60aedcb83b8902c705057aba
ShouvikMisra <shouvik.misra@keross.com>
2026-03-11 16:56:14 +0530

    outlet add to route perfectly

--- src/ikoncomponents/provider-wrapper/index.tsx
+++ src/ikoncomponents/provider-wrapper/index.tsx
@@ -5,7 +5,7 @@ import { RadiusProvider } from "../../utils/border-radius-provider";
 import { BreadcrumbProvider } from "../app-breadcrumb/BreadcrumbProvider";
 import { MainLayout } from "../main-layout";
 import { RefreshProvider } from "../main-layout/RefreshContext";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Outlet } from "react-router";
 import { TanstackProvider } from "./TanstackProvider";
 import { UserProvider } from "../../utils/userContext";
 import { getConfig } from "../../utils/config";
@@ -31,7 +31,10 @@ export function ProviderWrapper({ children }: { children: React.ReactNode }) {
               <BreadcrumbProvider>
                 <RefreshProvider>
                   <BrowserRouter>
-                    <MainLayout>{children}</MainLayout>
+                    <MainLayout>
+                      <Outlet />
+                      {children}
+                    </MainLayout>
                   </BrowserRouter>
                 </RefreshProvider>
               </BreadcrumbProvider>
